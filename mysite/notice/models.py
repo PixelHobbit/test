e55f28c865c7bb5df6c5491510f6b5aa5a8a1b44
Yihuan Shao <shaoyihuan2@gmail.com>
2019-02-06 17:28:08 +0800

    完善+
    stars未完成

--- mysite/notice/models.py
+++ mysite/notice/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 class Notice(models.Model):
     title = models.CharField(max_length=50)
     date = models.DateField()
-    text = models.TextField()
+    text = RichTextUploadingField()
 
     def __str__(self):
        return self.title
